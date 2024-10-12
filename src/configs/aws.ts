9379a7bda326dc6fd6e7210120531ef922f2777c
prakashgb1991 <66585983+prakashgb1991@users.noreply.github.com>
2024-10-13 09:57:38 +1100

    Update aws.ts

--- src/configs/aws.ts
+++ src/configs/aws.ts
@@ -2,4 +2,5 @@ export const COGNITO = {
   REGION: "us-east-1",
   USER_POOL_ID: "us-east-1_TnCk3eWzO",
   APP_CLIENT_ID: "4i6mvpqc34ublb9pisgsnv8u0r",
+  ID_POOL_ID: "us-east-1:5baf6ed8-06af-4aa4-b0fe-2c37272c2c32"
 };
